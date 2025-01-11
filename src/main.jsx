228b144648cdc7339b6b89e20f4f1d13fb340325
MinD4223 <minhduc04022003@gmail.com>
2025-01-11 23:45:24 +0700

    add again

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-<<<<<<< HEAD
   <BrowserRouter >
-=======
-  <BrowserRouter basename="/">
->>>>>>> 21348b48895d2466503d4a0818adac0a00e27cba
     <App />
   </BrowserRouter>
 );
